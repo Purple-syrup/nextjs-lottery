94b48a3f632d4aea561bed22fb4f612ef2701894
Purple-syrup <godhanded0@gmail.com>
2022-10-18 13:46:03 +0000

    out

---
+++ out/_next/static/8SSXQK82pa8hMNJkGA60r/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-f13e010c116e54e6.js"],"/_error":["static/chunks/pages/_error-a51993fe870ec2c8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
